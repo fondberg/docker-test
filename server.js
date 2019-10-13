5b35ecedb70c34093f75ad64a0deb1dbb8f3e6f1
Niklas Fondberg <niklas.fondberg@photowall.se>
2019-10-13 10:56:36 +0200

    added code

---
+++ server.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 80;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  console.log('Got request', JSON.stringify(req.connection.remoteAddress, null, 2));
+  res.send('Hello world 123\n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
